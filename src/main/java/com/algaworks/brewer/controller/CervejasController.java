4194cd826590722c698a46b9edfa624984bfc1d5
Andre Bronca <bronca.andre@gmail.com>
2018-04-28 17:29:45 -0300

    Ajustando a tela de cadastro

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -32,8 +32,5 @@ public class CervejasController {
 		return "redirect:/cervejas/novo";
 	}
 	
-	@RequestMapping("/cervejas/cadastro")
-	public String cadastro() {
-		return "cerveja/cadastro-produto";
-	}
+	
 }
