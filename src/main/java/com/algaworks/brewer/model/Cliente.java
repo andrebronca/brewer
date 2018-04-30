d09b0f6014dc0a4c972408a6929a2fe8735c2317
Andre Bronca <bronca.andre@gmail.com>
2018-04-30 10:54:04 -0300

    Cadastro de cliente, formulário, model, controller

---
+++ src/main/java/com/algaworks/brewer/model/Cliente.java
@@ -0,0 +1,86 @@
+package com.algaworks.brewer.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+public class Cliente {
+
+	private Long id;
+	
+	@NotBlank(message = "Nome é obrigatório!")
+	private String nome;
+	private String tipo;
+	private String cpfCnpj;
+	private String telefone;
+	private String email;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String cep;
+//	private Estado estado;
+//	private Cidade cidade;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+	public void setCpfCnpj(String cpfCnpj) {
+		this.cpfCnpj = cpfCnpj;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	
+	
+}
