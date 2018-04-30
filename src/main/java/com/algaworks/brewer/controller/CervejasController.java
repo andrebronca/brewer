25ddfd0f4b81a03cba7602d2504bdcd42e8f6c76
Andre Bronca <bronca.andre@gmail.com>
2018-04-30 19:04:39 -0300

    Adicionando e testando Log4j

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -2,6 +2,8 @@ package com.algaworks.brewer.controller;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,9 +15,16 @@ import com.algaworks.brewer.model.Cerveja;
 
 @Controller
 public class CervejasController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
 
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Objeto cerveja: "+ cerveja);
+		}
+		
 		return "cerveja/CadastroCerveja";
 	}
 	
@@ -25,6 +34,7 @@ public class CervejasController {
 			Model model, 
 			RedirectAttributes attributes) {
 		if (result.hasErrors()) {
+			logger.error("Tem erros: "+ result.toString());
 			return novo(cerveja);
 		}
 
