927ffe81934449ef55a02fd6660220ab7997989e
Andre Bronca <bronca.andre@gmail.com>
2018-04-30 15:29:37 -0300

    Tela de Estilo, model, controller

---
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -0,0 +1,16 @@
+package com.algaworks.brewer.model;
+
+public class Estilo {
+
+	private String estilo;
+
+	public String getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(String estilo) {
+		this.estilo = estilo;
+	}
+	
+	
+}
