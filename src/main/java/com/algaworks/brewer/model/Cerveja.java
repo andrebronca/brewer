02aef1b9b066fbadd37c121cc3c7f9e40ac8ad20
Andre Bronca <bronca.andre@gmail.com>
2018-05-01 15:03:46 -0300

    Inclusão de dependencias do hibernate, annotations para a classe Cerveja e  Estilo, relacionamentos, enums

--- src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -1,11 +1,32 @@
 package com.algaworks.brewer.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotBlank;
 
-public class Cerveja {
+@Entity
+@Table(name = "cerveja")
+public class Cerveja implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
@@ -16,7 +37,90 @@ public class Cerveja {
 	@Size(min=4, max=50, message="Tamanho da descrição deve estar entre: 4 e 50 caracteres")
 	private String descricao;
 	
+	private BigDecimal valor;
 	
+	@Column(name = "teor_alcoolico")
+	private BigDecimal teorAlcoolico;
+	
+	private BigDecimal comissao;
+	
+	@Column(name = "quantiddade_estoque")
+	private Integer quantidadeEstoque;
+	
+	@Enumerated(EnumType.STRING)
+	private Origem origem;
+	
+	@Enumerated(EnumType.STRING)
+	private Sabor sabor;
+	
+	@ManyToOne							//Muitos estilos para uma cerveja
+	@JoinColumn(name = "id_estilo")		//nome do campo na tabela
+	private Estilo estilo;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public BigDecimal getTeorAlcoolico() {
+		return teorAlcoolico;
+	}
+
+	public void setTeorAlcoolico(BigDecimal teorAlcoolico) {
+		this.teorAlcoolico = teorAlcoolico;
+	}
+
+	public BigDecimal getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(BigDecimal comissao) {
+		this.comissao = comissao;
+	}
+
+	public Integer getQuantidadeEstoque() {
+		return quantidadeEstoque;
+	}
+
+	public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+		this.quantidadeEstoque = quantidadeEstoque;
+	}
+
+	public Origem getOrigem() {
+		return origem;
+	}
+
+	public void setOrigem(Origem origem) {
+		this.origem = origem;
+	}
+
+	public Sabor getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(Sabor sabor) {
+		this.sabor = sabor;
+	}
+
+	public Estilo getEstilo() {
+		return estilo;
+	}
+
+	public void setEstilo(Estilo estilo) {
+		this.estilo = estilo;
+	}
+
 	public String getSku() {
 		return sku;
 	}
@@ -44,6 +148,31 @@ public class Cerveja {
 	@Override
 	public String toString() {
 		return "Cerveja [sku=" + sku + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
--- src/main/java/com/algaworks/brewer/model/Estilo.java
+++ src/main/java/com/algaworks/brewer/model/Estilo.java
@@ -1,16 +1,72 @@
 package com.algaworks.brewer.model;
 
-public class Estilo {
+import java.io.Serializable;
+import java.util.List;
 
-	private String estilo;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-	public String getEstilo() {
-		return estilo;
+@Entity
+@Table(name = "estilo")
+public class Estilo implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@OneToMany(mappedBy = "estilo")		//nome do atributo na classe Cerveja que faz o mapeamento
+	private List<Cerveja> cervejas;
+
+	public Long getId() {
+		return id;
 	}
 
-	public void setEstilo(String estilo) {
-		this.estilo = estilo;
+	public void setId(Long id) {
+		this.id = id;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estilo other = (Estilo) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
 	
 	
 }
